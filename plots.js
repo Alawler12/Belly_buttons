eaac15fc71bb1952f28b580fc030d872ad4b16c2
Alawler12 <ashleylawler12@gmail.com>
2020-10-16 15:04:31 -0400

    Module 12 Challenge

--- plots.js
+++ plots.js
@@ -1,67 +1,38 @@
-// // reads json file and prints it to the console
-// d3.json("samples.json").then(function(data){
-//     console.log(data);
-// });
-
-// //extract the wash frequency for each person in the array
-// d3.json("samples.json").then(function(data){
-//     wfreq = data.metadata.map(person => person.wfreq);
-//     console.log(wfreq);
-// });
-
-// //sort wash frequency in desceding order
-// d3.json("samples.json").then(function(data){
-//     wfreq = data.metadata.map(person =>
-// person.wfreq).sort((a,b) => b - a);
-//     console.log(wfreq);
-// });
-
-// //delete null values from the wash frequency array
-// d3.json("samples.json").then(function(data){
-//     wfreq = data.metadata.map(person =>
-// person.wfreq).sort((a,b) => b - a);
-//     filteredWfreq = wfreq.filter(element => element !=
-// null);
-//     console.log(filteredWfreq);
-// });
-
-// // display metadata for any individual in the dataset
-// d3.json("samples.json").then(function(data){
-//     firstPerson = data.metadata[0];
-//     Object.entries(firstPerson).forEach(([key, value]) =>
-//       {console.log(key + ': ' + value);});
-// });
-
-
-
 function init() {
-    data = [{
-      x: [1, 2, 3, 4, 5],
-      y: [1, 2, 4, 8, 16] }];
-    Plotly.newPlot("plot", data);
-  };
+    var selector = d3.select("#selDataset");
   
-  d3.selectAll("#dropdownMenu").on("change", updatePlotly);
-  function updatePlotly() {
-    var dropdownMenu = d3.select("#dropdownMenu");
-    var dataset = dropdownMenu.property("value");
-  
-    var xData = [1, 2, 3, 4, 5];
-    var yData = [];
-  
-    if (dataset === 'dataset1') {
-      yData = [1, 2, 4, 8, 16];
-    };
-  
-    if (dataset === 'dataset2') {
-      yData = [1, 10, 100, 1000, 10000];
-    };
-  
-    var trace = {
-      x: [xData],
-      y: [yData],
-    };
-    Plotly.restyle("plot", trace);
-  };
+    d3.json("samples.json").then((data) => {
+      console.log(data);
+      var sampleNames = data.names;
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+  })}
   
   init();
+
+function optionChanged(newSample) {
+  buildMetadata(newSample);
+  buildCharts(newSample);
+ }
+
+ function buildMetadata(sample) {
+  d3.json("samples.json").then((data) => {
+    var metadata = data.metadata;
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+    var PANEL = d3.select("#sample-metadata");
+
+    PANEL.html("");
+    PANEL.append("h6").text('ID: ' + result.id);
+      PANEL.append("h6").text('ETHNICITY: ' + result.ethnicity);
+      PANEL.append("h6").text('GENDER: ' + result.gender);
+      PANEL.append("h6").text('AGE: ' + result.age);
+      PANEL.append("h6").text('LOCATION: ' + result.location);
+      PANEL.append("h6").text('BBTYPE: ' + result.bbtype);
+      PANEL.append("h6").text('WFREQ: ' + result.wfreq);
+  });
+}
